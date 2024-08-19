248b91b5031137095783f5e750269caf5ae33c7c
ArthurNoteCRE <arthurfsilva1985@gmail.com>
2024-08-19 15:23:50 -0300

    Finalizado a atividade 1

--- Atividade 1/main.dart
+++ Atividade 1/main.dart
@@ -1,73 +1,91 @@
 import 'dart:io';
 
-void calcularPerimetro(String figura, va1, [va2, va3]){
+void calcularPerimetro(String figura, int va1, [ int va2 = 0, int va3 = 0]){
 
   double pi = 3.14;
-  int v1 = int.parse(va1);
-  int v2 = int.parse(va2);
-  int v3 = int.parse(va3);
   
   switch(figura){
 
     case '1':
-      int resultado = v1 + v2 + v3;
+      int resultado = va1 + va2 + va3;
       print('O perimetro do seu Triangulo é $resultado');
       break;
 
     case '2':
-      var resultado = 2 * pi * v1;
+      var resultado = 2 * pi * va1;
       print('O perimetro do seu Circulo é $resultado');
       break;
 
     case '3':
-      var resultado = 4 * v1;
+      var resultado = 4 * va1;
       print ('O perimetro do seu Quadrado é $resultado');
       break;
 
     case '4':
-      var resultado = 2 * v1 + 2 *v2;
+      var resultado = 2 * va1 + 2 *va2;
       print('O perimetro do seu retangulo é $resultado');
       break;
   }
 
 }
 
+int lerValores(String descricao){
+    while(true){
+      print(' Digite o valor para $descricao:');
+      int? valor = int.tryParse(stdin.readLineSync()!);
+      if (valor != null && valor > 0){
+        return valor;
+      } else {
+        print ('Valor invalido. Tente novamente!');
+      }
+    }
+}
+
 
 void main(List<String> args) {
-    print("Qual objeto você gostaria de calcular?\n 1-Triângulo \n 2-Circulo\n 3-Quadrado\n 4-Retangulo");
-    String? respostaMenu = stdin.readLineSync();
-    
-    switch(respostaMenu){
 
-      case '1':
-        print("Sua resposta foi 1-Triangulo");
-        print('Digite os três lados do triangulo: ');
-        var v1 = stdin.readLineSync();
-        var v2 = stdin.readLineSync();
-        var v3 = stdin.readLineSync();
-        calcularPerimetro('1', v1, v2, v3);
-        break;
-      case '2':
-        print("Sua resposta foi 2-Circulo");
-        print('Digite o Raio do círculo');
-        var v1 = stdin.readLineSync();
-        calcularPerimetro('2', v1, 0, 0);
-        break;
-      case '3':
-        print("Sua resposta foi 3-Quadrado");
-        print('Digite o tamanho do lado do quadrado: ');
-        var v1 = stdin.readLineSync();
-        calcularPerimetro('3', v1, 0 , 0);
-        break;
-      case '4':
-        print("Sua resposta foi 4-Retangulo");
-        print('Digite a base e a altura respectivamente: ');
-        var v1 = stdin.readLineSync();
-        var v2 = stdin.readLineSync();
-        calcularPerimetro('4', v1, v2, 0);
-        break;
-      default:
-        print("Resposta indisponível");
-        break;
-    }
+  String? respostaMenu = '';
+   
+    
+    do{
+       print("Qual objeto você gostaria de calcular?\n 1-Triângulo \n 2-Circulo\n 3-Quadrado\n 4-Retangulo\n 0 - Sair do sistema");
+      respostaMenu = stdin.readLineSync();
+
+      switch(respostaMenu){
+
+            case '1':
+              print("Sua resposta foi 1-Triangulo");
+              print('Digite os três lados do triangulo: ');
+              var v1 = lerValores('primeiro lado do quadrado');
+              var v2 = lerValores('segundo ladro do quadrado');
+              var v3 = lerValores('terceiro lado do quadrado');
+              calcularPerimetro('1', v1, v2, v3);
+              break;
+            case '2':
+              print("Sua resposta foi 2-Circulo");
+              var v1 = lerValores('Raio');
+              calcularPerimetro('2', v1, 0, 0);
+              break;
+            case '3':
+              print("Sua resposta foi 3-Quadrado"); 
+              var v1 = lerValores('lado do quadrado');
+              calcularPerimetro('3', v1, 0 , 0);
+              break;
+            case '4':
+              print("Sua resposta foi 4-Retangulo");
+              print('Digite a base e a altura respectivamente: ');
+              var v1 = lerValores('base do retangulo');
+              var v2 = lerValores('altura do retangulo');
+              calcularPerimetro('4', v1, v2, 0);
+              break;
+            case '0':
+              print('O sistema será encerrado! ');
+              break;
+            default:
+              print("Resposta indisponível");
+              break;
+          }
+    } while (respostaMenu != '0' );
+
+    
 }
